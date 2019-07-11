3db8c530dc19874f84ae1bf6ea796ebcf172aee4
Alexander Sodt <alexander.sodt@nih.gov>
2019-07-11 12:49:30 -0400

    added 2D-2D RD files

---
+++ src/fpr_subroutines/2D.C
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <cmath>
+#include <gsl/gsl_integration.h>
+#include <gsl/gsl_matrix.h>
+#include <gsl/gsl_sf_bessel.h>
+#include <gsl/gsl_vector.h>
+#include "2Drelated.h"
+#include <gsl/gsl_errno.h>
+#include <gsl/gsl_odeiv.h>
+#include <fstream>
+#include <iostream>
+#include <limits>
+#include <math.h>
+#include <cfloat>
+
+typedef struct
+{
+	int nEntries;
+	int nEntriesSpace;
+	int DDtableindex;
+	double *TBLID;
+	gsl_matrix **contsur;
+	gsl_matrix **contnorm;
+	gsl_matrix **contpir;
+	int *vecLen;
+}  twoDReactionPTable;
+
+// DDtableindex: index of where we store information about a 2D reaction.
+
+static double tolKvalue = 1e-3; // correct this. depends on units, machine precision etc.
+static double tolDvalue = 1e-3;
+//
+//  copied and reorganized slightly from Margaret Johnson's code.
+//
+
+double get_2D_2D_rxn_prob( double R1, double kr, double bindrad, double Dtot, twoDReactionPTable *rxnTable, double deltat, double Rmax )
+{
+	double ktemp = kr / 2 / bindrad;
+	int uniquetableindex = -1;
+	int tableexistflag = 0;
+
+	for( int idd = 0; idd < rxnTable->nEntries; idd++ )
+	{
+		if( fabs(rxnTable->TBLID[2*idd+0]-ktemp) < tolKvalue && fabs(rxnTable->TBLID[2*idd+1]-Dtot) < tolDvalue )
+		{
+			tableexistflag = 1;
+			uniquetableindex = idd;
+			break;
+		}
+	}
+	
+	int vL=0;
+
+	if( uniquetableindex == -1 )
+	{
+		// compute table.
+
+		if( rxnTable->nEntries == rxnTable->nEntriesSpace )
+		{
+			rxnTable->TBLID = (double *)realloc( rxnTable->TBLID, sizeof(double) * 2 * rxnTable->nEntriesSpace );
+			rxnTable->vecLen = (int *)realloc( rxnTable->vecLen, sizeof(int) * rxnTable->nEntriesSpace );
+			rxnTable->contsur = (gsl_matrix **)realloc( rxnTable->contsur, sizeof(gsl_matrix *) * rxnTable->nEntriesSpace );
+			rxnTable->contnorm = (gsl_matrix **)realloc( rxnTable->contnorm, sizeof(gsl_matrix *) * rxnTable->nEntriesSpace );
+			rxnTable->contpir = (gsl_matrix **)realloc( rxnTable->contpir, sizeof(gsl_matrix *) * rxnTable->nEntriesSpace );
+		} 
+
+		uniquetableindex = rxnTable->nEntries;
+
+		rxnTable->TBLID[2*uniquetableindex+0] = ktemp;
+		rxnTable->TBLID[2*uniquetableindex+1] = Dtot;
+
+		rxnTable->vecLen[uniquetableindex] = sizelookup(bindrad, Dtot, deltat, Rmax);
+
+		vL = rxnTable->vecLen[uniquetableindex];
+		rxnTable->contsur[uniquetableindex] = gsl_matrix_alloc(2,vL); 
+		rxnTable->contnorm[uniquetableindex] = gsl_matrix_alloc(2,vL); 
+		rxnTable->contpir[uniquetableindex] = gsl_matrix_alloc(vL,vL); 
+		      
+		DDmatrixcreate(rxnTable->contsur[uniquetableindex], rxnTable->contnorm[uniquetableindex], rxnTable->contpir[uniquetableindex], bindrad, Dtot, ktemp, deltat, Rmax);
+
+		rxnTable->nEntries++;
+	}
+		    
+	double probvec1 = DDpsur(rxnTable->contsur[uniquetableindex], Dtot, deltat, R1, bindrad);
+
+	return probvec1;
+}
+
+
